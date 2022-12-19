82cbed27799ac6c95ad5e20f593108077b68ed2d
mizan192 <miz1998an@gmail.com>
2022-12-19 06:47:31 +0600

    dsu on find path

--- Find if Path Exists in Graph.cpp
+++ Find if Path Exists in Graph.cpp
@@ -1,3 +1,41 @@
+//dfs code will use more time and memeory than dsu
+class Solution {
+public:
+ static const int MAX = 200003;
+  int pr[MAX+2];
+    int find_parent(int u) {
+	if (u == pr[u]) return u;
+	return pr[u] = find_parent(pr[u]);
+    }
+
+void union_set(int u, int v) {
+	int pu = find_parent(u);
+	int pv = find_parent(v);
+	pr[pu] = pv;
+}
+  
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+        for(int i=1;i<=n;i++) {
+            pr[i]=i;
+        }
+
+        for(auto x : edges){
+            int u=x[0],v=x[1];
+            union_set(u,v);
+        }
+
+        int parentSource=find_parent(source);
+        int parentDestination=find_parent(destination);
+
+        if(parentSource==parentDestination) return 1;
+        else return 0;
+       
+
+    }
+};
+
+
+
 class Solution {
 public:
  static const int MAX = 200003;
